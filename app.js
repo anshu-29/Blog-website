f175c8622ff40352564ed2fd6dba936c83f3f6e1
Aakansh Arora <aakansharora2909@gmail.com>
2023-07-05 02:06:40 +0530

    Adding image,video and audio uploading options that can be stored in database

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose")
 const _ = require('lodash');
+const multer = require ("multer")
+const storage = multer.memoryStorage();
+const upload = multer({storage:storage})
 
 const homeStartingContent = "Welcome to my blog Website. Here you can put your thoughts and See them being published on the website";
 const aboutContent = "I created this Website as a practice project to test my skills on NodeJs, Express, EJS Templating and I will be publishing this web app along with MONGODB linkage soon";
@@ -21,13 +24,12 @@ app.use(express.static("public"));
  mongoose.connect("mongodb://127.0.0.1:27017/blogDB",{useNewUrlParser:true})
 const postSchema = {
     title: String,
-    content:String
+    content:String,
+    imageData: Buffer,
+    videoData: Buffer,
+    audioData: Buffer
 }
 const Post = mongoose.model("Post",postSchema)
-
-
-
-
 
 app.get("/", async function (req, res) {
     try {
@@ -58,29 +60,33 @@ app.get("/compose",function (req, res) {
 
 
 
-app.post("/compose",function (req,res) {
-
-
-    const post = new Post ({
-        title : req.body.composeInput,
-        content : req.body.composeBox,
+app.post("/compose", upload.fields([{ name: 'imageFile'}, { name: 'videoFile'}, { name: 'audioFile'}]), function (req, res) {
+    const post = new Post({
+        title: req.body.composeInput,
+        content: req.body.composeBox,
+        imageData: req.files['imageFile'][0].buffer,
+        videoData: req.files['videoFile'] ? req.files['videoFile'][0].buffer : null,
+        audioData: req.files['audioFile'] ? req.files['audioFile'][0].buffer : null
     });
-post.save().then(r=>{
-    res.redirect("/");
+
+    post.save().then((r) => {
+        res.redirect("/");
+    });
 });
 
-
-});
 
 
 app.get("/posts/:postId", async function (req, res) {
     const requestedId = req.params.postId;
 
     try {
-        const post = await Post.find({ _id: requestedId });
+        const post = await Post.findById(requestedId);
         res.render("post", {
-            postedTitle: post[0].title,
-            postContent: post[0].content
+            postedTitle: post.title,
+            postContent: post.content,
+            imageData: post.imageData,
+            videoData: post.videoData,
+            audioData: post.audioData
         });
     } catch (err) {
         console.log(err);
